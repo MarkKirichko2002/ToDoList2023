fb031a0dbc4dea0f5eea6febfa8928c632d62a88
MarkDestroyer <markkir123@gmail.com>
2023-05-30 23:08:01 +0300

    update
    Added ContextMenu.

--- ToDoList/ToDoList/VIPER Modules/ToDoList VIPER/View/Cell/ToDoListItemTableViewCell.swift
+++ ToDoList/ToDoList/VIPER Modules/ToDoList VIPER/View/Cell/ToDoListItemTableViewCell.swift
@@ -11,6 +11,7 @@ protocol CustomCellDelegate: AnyObject {
     func didTapEdit(item: ToDoListItemModel)
     func didTapEditImage(item: ToDoListItemModel)
     func didTapCompleteStatus(item: ToDoListItemModel, complete: Bool)
+    func didTapDelete(item: ToDoListItemModel)
 }
 
 class ToDoListItemTableViewCell: UITableViewCell {
@@ -19,21 +20,12 @@ class ToDoListItemTableViewCell: UITableViewCell {
     weak var delegate: CustomCellDelegate?
     private var item: ToDoListItemModel?
     private var isCompleted = false
-
+    
     @IBOutlet var TitleLabel: UILabel!
     @IBOutlet var DateLabel: UILabel!
     @IBOutlet var ItemImage: UIImageView!
     @IBOutlet var ItemComplete: UIButton!
-    
-    @IBAction func EditToDoListItem() {
-        guard let item = item else {return}
-        delegate?.didTapEdit(item: item)
-    }
-    
-    @IBAction func EditToDoListItemImage() {
-        guard let item = item else {return}
-        delegate?.didTapEditImage(item: item)
-    }
+    @IBOutlet var ContextButton: UIButton!
     
     @IBAction func ToggleCompleteStatus() {
         guard let item = item else {return}
@@ -52,5 +44,32 @@ class ToDoListItemTableViewCell: UITableViewCell {
         TitleLabel.text = item.title
         DateLabel.text = item.date
         ItemComplete.setImage(UIImage(systemName: item.complete ? "checkmark.circle.fill" : "checkmark.circle"), for: .normal)
+        setUpContextMenu()
+    }
+    
+    private func setUpContextMenu() {
+        // редактировать элемента
+        let edit = UIAction(title: "изменить", image: UIImage(systemName: "square.and.pencil")) { _ in
+            guard let item = self.item else {return}
+            DispatchQueue.main.async {
+                self.delegate?.didTapEdit(item: item)
+            }
+        }
+        // редактировать фото
+        let editImage = UIAction(title: "фото", image: UIImage(systemName: "photo")) { _ in
+            guard let item = self.item else {return}
+            DispatchQueue.main.async {
+                self.delegate?.didTapEditImage(item: item)
+            }
+        }
+        // удалить элемента
+        let delete = UIAction(title: "удалить", image: UIImage(systemName: "trash")) { _ in
+            guard let item = self.item else {return}
+            DispatchQueue.main.async {
+                self.delegate?.didTapDelete(item: item)
+            }
+        }
+        ContextButton.showsMenuAsPrimaryAction = true
+        ContextButton.menu = UIMenu(title: "", children: [edit, editImage, delete])
     }
 }
